7992eb05453ac29f28d5a9f0c0e4355a191acb6f
Muskan Tariq <muskantariq2003@gmail.com>
2026-08-30 15:57:36 +0500

    Simple chatbot backend

---
+++ 11_chatbot_backend.py
@@ -0,0 +1,34 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="openai/gpt-oss-120b",
+    temperature=0,    
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {'messages':[response]}
+
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
