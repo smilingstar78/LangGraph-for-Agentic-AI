db32184537dfa6df24f25dac0d0a52c101961112
Muskan Tariq <muskantariq2003@gmail.com>
2026-09-09 18:12:59 +0500

    Backened Chatbot

--- 11_chatbot_backend.py
+++ 11_chatbot_backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
@@ -32,3 +32,11 @@ graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+for message_chunk, metadata in chatbot.stream(
+    {'messages':[HumanMessage(content=user_input)]},
+    config = {'configurable' : {'thread_id':'thread-1'}},
+    stream_mode='messages'
+):
+    if message_chunk.content:
+        print(message_chunk.content, end=" ", flush=True)
