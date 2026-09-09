a92dc054f3f51a831bfee58da5d896290b5e0bf6
Muskan Tariq <muskantariq2003@gmail.com>
2026-09-09 18:30:21 +0500

    Added streaming

--- 11_chatbot_frontend.py
+++ 11_chatbot_frontend.py
@@ -1,34 +1,60 @@
 import streamlit as st
-from langgraph_backend import chatbot
+
+from chatbot_backend import chatbot
 from langchain_core.messages import HumanMessage
 
-# st.session_state -> dict -> 
+
 CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
+
+# st.session_state -> dict
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
-# loading the conversation history
+
+# Loading the conversation history
 for message in st.session_state['message_history']:
+
     with st.chat_message(message['role']):
         st.text(message['content'])
 
-#{'role': 'user', 'content': 'Hi'}
-#{'role': 'assistant', 'content': 'Hi=ello'}
 
+# Get user input
 user_input = st.chat_input('Type here')
+
 
 if user_input:
 
-    # first add the message to message_history
-    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    # Add user message to history
+    st.session_state['message_history'].append({
+        'role': 'user',
+        'content': user_input
+    })
+
+    # Display user message
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
-    
-    ai_message = response['messages'][-1].content
-    # first add the message to message_history
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+
+    # Generate AI response
     with st.chat_message('assistant'):
-        st.text(ai_message)
+
+        ai_message = st.write_stream(
+            message_chunk.content
+            for message_chunk, metadata in chatbot.stream(
+                {
+                    'messages': [
+                        HumanMessage(content=user_input)
+                    ]
+                },
+                config=CONFIG,
+                stream_mode='messages'
+            )
+        )
+
+
+    # Add AI response to history
+    st.session_state['message_history'].append({
+        'role': 'assistant',
+        'content': ai_message
+    })
